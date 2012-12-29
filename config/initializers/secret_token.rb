f5f9ffe67b30afb8ce19478d9e5efaf663e5db6a
emilien <emilien.charbonnier@free.fr>
2012-12-29 07:19:42 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsana::Application.config.secret_token = '43a20145f3bb84aa10df1c125d20eccc1ab7c4f46d5dd4895ffca376224ac3aca75a171ff129e0d756214ab6748efa2ae20dfda26a8c217aa93688c2e0c0b5ab'
